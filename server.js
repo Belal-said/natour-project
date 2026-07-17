0b23b3180d4f6b47359d7f9124f3c4491349f9bb
Belal-said <belalelkholy33@gmail.com>
2026-07-17 10:46:11 -0700

    Setup hosted database

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
+const dns = require("dns");
 
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 dotenv.config({ path: "./config.env" });
 
-console.log(process.env);
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log("DB connected successfully");
+  });
+
+
 
 // Running the app ( server )
 const port = process.env.PORT || 3500;
