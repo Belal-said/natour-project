59e8158863e883b55495f9cc19747744992a5f33
Belal-said <belalelkholy33@gmail.com>
2026-07-17 11:03:10 -0700

    Creating a Simple Tour Model

--- server.js
+++ server.js
@@ -13,7 +13,25 @@ mongoose
     console.log("DB connected successfully");
   });
 
+// Creating Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
 
+// Creating Schema Model
+const Tour = mongoose.model('Tour', tourSchema);
 
 // Running the app ( server )
 const port = process.env.PORT || 3500;
