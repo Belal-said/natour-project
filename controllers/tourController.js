44bc2d5204d686927319ab5493c4f175a2bb1935
belal-said <belalelkholy33@gmail.com>
2026-07-19 01:58:28 +0300

    Getting all and one

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -31,36 +31,46 @@ const Tour = require('./../models/tourModel.js')
 // }
 
 // get request method under routing
-exports.getAllTours = (req, res) => {
-  // req, res function here is called route handler
-  res.status(200).json({
-    // status: "success",
-    // requestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status : "success",
+      data : {
+        tours
+      }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err
+    })
+  }
 };
 
 // get one tour
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    // res.json sends a json response to the client.
-    // status: "success",
-    // data: {
-    //   tour,
-    // },
-  });
+exports.getTour = async (req, res) => {
+  try{
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 };
 
 // create tour
 exports.createTour = async (req, res) => {
-  console.log(`create tour function called`)
-  console.log(req.body)
   try {
     const newTour = await Tour.create(req.body)
-    console.log(newTour)
     res.status(200).json({
       status: "success",
       data: {
