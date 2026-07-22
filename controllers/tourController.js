993829d05040465921126a2c73bd2b6ae0bed2e3
Belal-said <belalelkholy33@gmail.com>
2026-07-22 13:01:54 -0700

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -51,17 +51,31 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      query = query.sort("-createdAt");
     }
 
     // 3) Field Limiting
     if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select('name duration price')
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select("name duration price");
     } else {
-      query = query.select('-__v')
+      query = query.select("-__v");
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page does not exist')
+      }
+    }
+    
     // Execute API
     const tours = await query;
 
